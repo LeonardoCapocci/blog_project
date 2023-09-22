e81a361462706811edf94b5100c27bee1dd00555
LeonardoCapocci <leocap2001@gmail.com>
2023-09-21 18:14:34 -0700

    Update style of home, single blog, login, register

--- blogs/views.py
+++ blogs/views.py
@@ -8,7 +8,9 @@ from .forms import BlogForm, PostForm
 def index(request):
     """The home page for blogs."""
     posts = Post.objects.order_by('-date_added')
-    context = {'posts':posts}
+    for post in posts:
+        blog = post.blog
+    context = {'posts':posts, 'blog':blog}
     return render(request, 'blogs/index.html', context)
 
 def blogs(request):
